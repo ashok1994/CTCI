774b608bd7350eee7fede2f645b6264bd6bd0dae
Ashok Mandal <ashok1994>
2017-08-16 22:35:22 +0530

    LinkedList01Edited

--- LinkedLists/remove_dups/remove_dups.py
+++ LinkedLists/remove_dups/remove_dups.py
@@ -5,74 +5,15 @@
     How would you solve this problem if a temporary buffer is not allowed ? --> remove_dups_b()
 """
 
-import unittest
+from LinkedLists.LinkedList import LinkedList
 
-class Node:
-    def __init__(self, number):
-        self.data = number
-        self.next = None
-
-    def __str__(self):
-        return "Node({})".format(self.data)
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def __str__(self):
-        # Returns formatted LinkedList
-        if self.head is None:
-            return "(empty)"
-        else:
-            string = ""
-
-            # First Node
-            curr = self.head
-            string += str(curr.data)+" -> "
-
-            # Iterate through all nodes
-            while curr.next is not None:
-                curr = curr.next
-                string += str(curr.data)+" -> "
-
-            # Null pointer of last element
-            string += "(None)"
-
-            return string
-
-    def append_to_tail(self, number):
-        # Append a new element to the end of the linked list
-        new_node = Node(number)
-
-        if self.head is None:
-            self.head = new_node
-        else:
-            curr = self.head
-            # Iterate through list , until we reach end
-            while curr.next is not None:
-                curr = curr.next
-            curr.next = new_node
-
-    def return_list(self):
-        if self.head is None:
-            return []
-        else:
-            arr = []
-            curr = self.head
-            while curr is not None:
-                arr.append(curr.data)
-                curr = curr.next
-            return arr
-def remove_dups(linked_list:LinkedList):
+def remove_dups(linked_list):
     memo = []
     curr = linked_list.head
     # First Value
     memo.append(curr.data)
 
     while curr.next is not None:
-
-
         if curr.next.data in memo:
             curr.next = curr.next.next
         else:
@@ -80,32 +21,27 @@ def remove_dups(linked_list:LinkedList):
             curr = curr.next
 
 
-    return linked_list
+def remove_dups_follow_up(linked_list):
+    i = linked_list.head
+    j = i
+
+    while i is not None:
+        while j is not None:
+            if j.next:
+                if j.next.data == i.data:
+                    j.next = j.next.next
+                else:
+                    j = j.next
+            else:
+                j = j.next
+        i = i.next
+        j = i
 
 
-def create_linked_list(array:list):
-    my_linked_list = LinkedList()
-    for value in array:
-        my_linked_list.append_to_tail(value)
-    return my_linked_list
+ll = LinkedList([1,3,5,3,2, 7,7,2,5, 8, 9])
 
+print(ll)
 
+remove_dups_follow_up(ll)
 
-
-class TestRemoveDups(unittest.TestCase):
-    data = [
-        ( [1,2,3,4,5,5] , [1,2,3,4,5] ),
-        ( [1,2,3,2,2,5,6] , [1, 2,3,5,6])
-    ]
-
-    def test_remove_dups(self):
-        for [array, expected] in self.data:
-            temp_linked_list = create_linked_list(array)
-            remove_dups(temp_linked_list)
-            self.assertEqual(expected, temp_linked_list.return_list())
-
-
-if __name__ == "__main__":
-    unittest.main()
-
-
+print(ll)
