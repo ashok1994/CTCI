482042de67fa445ffe3f4757ed868a2efee04139
Ashok Mandal <askipop@gmail.com>
2017-11-09 23:50:23 +0530

    LinkedLists07 Improved

--- LinkedLists/intersection/intersection.py
+++ LinkedLists/intersection/intersection.py
@@ -7,17 +7,31 @@
 
 from LinkedLists.LinkedList import LinkedList
 
-def get_intersecting_node(ll1, ll2):
-    head1 = ll1.head
-    while head1:
-        head2 = ll2.head
-        while head2:
-            if head1 is head2:
-                return head1
-            else:
-                head2 = head2.next
+def get_node_uniform(head1, head2):
+    while head1 is not head2:
         head1 = head1.next
-    return False
+        head2 = head2.next
+    return head1
+
+
+def adjust_head(n,head):
+    while n != 0:
+        head = head.next
+        n-=1
+    return head
+
+
+def get_intersecting_node(ll1, ll2):
+    len1 = len(ll1)
+    len2 = len(ll2)
+    if len1 == len2:
+        return get_node_uniform(ll1.head,ll2.head)
+    elif len2 > len1:
+        head2 = adjust_head(len2 - len1 , ll2.head)
+        return get_node_uniform(ll1.head, head2)
+    else:
+        head1 = adjust_head(len1 - len2, ll1.head)
+        return get_node_uniform(head1, ll2.head)
 
 
 
@@ -25,8 +39,8 @@ def get_intersecting_node(ll1, ll2):
 l1 = LinkedList()
 
 l1.add(1)
-l1.add(2)
-node = l1.add(3)
+node = l1.add(2)
+l1.add(3)
 l1.add(4)
 
 
@@ -34,9 +48,9 @@ l2 = LinkedList()
 l2.add(11)
 l2.add(12)
 l2.add(13)
-
 l2.tail.next = node
-l2.tail = node
+l2.tail = l1.tail
+
 
 print(get_intersecting_node(l1, l2))
 
